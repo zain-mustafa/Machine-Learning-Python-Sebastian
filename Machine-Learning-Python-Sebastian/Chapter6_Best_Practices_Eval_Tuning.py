e53c1b04f0a92dbb3c53c62ff3b3fe62d617322e
Zain Mustafa <zainmustafa@live.com>
2019-12-29 05:26:37 -0500

    Finished Chapter 6

--- Machine-Learning-Python-Sebastian/Chapter6_Best_Practices_Eval_Tuning.py
+++ Machine-Learning-Python-Sebastian/Chapter6_Best_Practices_Eval_Tuning.py
@@ -20,6 +20,11 @@ from sklearn.model_selection import StratifiedKFold
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import learning_curve
 from sklearn.model_selection import validation_curve
+from sklearn.model_selection import GridSearchCV
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import precision_score, recall_score, f1_score
 
 ############################
 # STREAMLINE WORKFLOWS WITH PIPELINES
@@ -130,6 +135,140 @@ plt.show()
 # FINE-TUNING MACHINE LEARNING MODELS VIA GRID SEARCH
 ############################
 
+pipe_svc = Pipeline([
+    ('scl', StandardScaler()),
+    ('clf', SVC(random_state=1))
+])
+
+# Using grid search to find the best hyper-parameters
+
+param_range = [0.0001, 0.001, 0.01, 0.1, 1.0, 10.0, 100.0, 1000.0]
+param_grid = [
+    {
+        'clf__C': param_range,
+        'clf__kernel': ['linear']
+    },
+    {
+        'clf__C': param_range,
+        'clf__gamma': param_range,
+        'clf__kernel': ['rbf']
+    }
+]
+gs = GridSearchCV(
+    estimator=pipe_svc,
+    param_grid=param_grid,
+    scoring='accuracy',
+    cv=10,
+    n_jobs=-1
+)
+gs = gs.fit(X_train, y_train)
+print(gs.best_score_)
+
+print(gs.best_params_)
+
+# Estimating the Performance
+
+clf = gs.best_estimator_
+clf.fit(X_train, y_train)
+print('Test accuracy: %.3f' % clf.score(X_test, y_test))
+
+# Using nested-cross Validation to select the best ML Algorithm for the problem
+
+# Using SVM
+
+gs = GridSearchCV(
+    estimator=pipe_svc,
+    param_grid=param_grid,
+    scoring='accuracy',
+    cv=2,
+    n_jobs=-1
+)
+
+scores = cross_val_score(
+    gs,
+    X_train,
+    y_train,
+    scoring='accuracy',
+    cv=5
+)
+
+print(
+    'CV accuracy: %.3f +/- %.3f' %
+    (np.mean(scores), np.std(scores))
+    )
+
+# Using Decision Tree Classifier
+
+gs = GridSearchCV(
+    estimator=DecisionTreeClassifier(random_state=0),
+    param_grid = [
+        {'max_depth': [1, 2, 3, 4, 5, 6, 7, None]}
+    ],
+    scoring='accuracy',
+    cv=5
+)
+
+scores = cross_val_score(
+    gs,
+    X_train,
+    y_train,
+    scoring='accuracy',
+    cv=5
+)
+
+print(
+    'CV accuracy: %.3f +/- %.3f' %
+    (np.mean(scores), np.std(scores))
+    )
+
+# Using 2 different algorithms and checking the accuracy through GridSearch allows us to see
+# which one of the two should be used.
+
 ############################
 # LOOKING AT DIFFERENT PERFORMANCE EVALUATIONS METRICS
 ############################
+
+# Producing the confusion Matrix
+
+pipe_svc.fit(X_train, y_train)
+y_pred = pipe_svc.predict(X_test)
+confmat = confusion_matrix(
+    y_true=y_test,
+    y_pred=y_pred
+)
+print(confmat)
+
+fig, ax = plt.subplots(figsize = (2.5, 2.5))
+ax.matshow(
+    confmat,
+    cmap=plt.cm.Blues,
+    alpha=0.3
+)
+for i in range(confmat.shape[0]):
+    for j in range(confmat.shape[1]):
+        ax.text(
+            x=j,
+            y=i,
+            s=confmat[i,j],
+            va='center',
+            ha='center'
+        )
+plt.xlabel('predicted label')
+plt.ylabel('true label')
+plt.show()
+
+# Precision, Recall and F1-Score through Scikit
+
+print(
+    'Precision: %.3f' %
+    precision_score(y_true=y_test, y_pred=y_pred)
+)
+print(
+    'Recall: %.3f' %
+    recall_score(y_true=y_test, y_pred=y_pred)
+)
+print(
+    'F1: %.3f' %
+    f1_score(y_true=y_test, y_pred=y_pred)
+)
+
