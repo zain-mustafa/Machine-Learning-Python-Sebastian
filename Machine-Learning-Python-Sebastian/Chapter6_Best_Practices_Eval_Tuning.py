b3e1f10e2f9c7d71f882eec346b1b847700a1c1f
Zain Mustafa <zainmustafa@live.com>
2019-12-29 04:25:08 -0500

    Chapter6: Validation Curves

--- Machine-Learning-Python-Sebastian/Chapter6_Best_Practices_Eval_Tuning.py
+++ Machine-Learning-Python-Sebastian/Chapter6_Best_Practices_Eval_Tuning.py
@@ -9,6 +9,7 @@
 
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
@@ -17,6 +18,8 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.pipeline import Pipeline
 from sklearn.model_selection import StratifiedKFold
 from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import learning_curve
+from sklearn.model_selection import validation_curve
 
 ############################
 # STREAMLINE WORKFLOWS WITH PIPELINES
@@ -69,9 +72,59 @@ print('CV accuracy: %.3f +/- %.3f' % (np.mean(scores), np.std(scores)))
 
 ############################
 # DEBUGGING ALGORITHMS WITH LEARNING AND VALIDATION TECHNIQUES
-############################
+###########################
 
+# Learning Curve
 
+pipe_lr = Pipeline([
+    ('scl', StandardScaler()),
+    ('clf', LogisticRegression(penalty='l2', random_state=0)),
+])
+
+train_sizes, train_scores, test_scores = learning_curve(estimator=pipe_lr, X=X_train, y=y_train, train_sizes=np.linspace(0.1, 1.0, 10), cv=10, n_jobs=1)
+
+train_mean = np.mean(train_scores, axis=1)
+train_std = np.std(train_scores, axis=1)
+test_mean = np.mean(test_scores, axis=1)
+test_std = np.std(test_scores, axis=1)
+
+plt.plot(train_sizes, train_mean, color='blue', marker='o', markersize=5, label='Training Accuracy')
+
+plt.fill_between(train_sizes, train_mean + train_std, train_mean - train_std, alpha=0.15, color='blue')
+
+plt.plot(train_sizes, test_mean, color='green', linestyle='--', marker='s', markersize=5, label='validation accuracy')
+
+plt.fill_between(train_sizes, test_mean + test_std, test_mean - test_std, alpha=0.15, color='green')
+
+plt.grid()
+plt.xlabel('Number of training samples')
+plt.ylabel('Accuracy')
+plt.legend(loc='lower right')
+plt.ylim([0.8, 1.0])
+plt.show()
+
+# Validation Curve
+
+param_range = [0.001, 0.01, 0.1, 1.0, 10.0, 100.0]
+train_scores, test_scores = validation_curve(estimator=pipe_lr, X=X_train, y=y_train, param_name='clf__C', param_range=param_range, cv=10)
+train_mean = np.mean(train_scores, axis=1)
+train_std = np.std(train_scores, axis=1)
+test_mean = np.mean(test_scores, axis=1)
+test_std = np.std(test_scores, axis=1)
+
+plt.plot(param_range, train_mean, color='blue', marker='o', markersize=5, label='training accuracy')
+plt.fill_between(param_range, train_mean + train_std, train_mean - train_std, alpha=0.15, color='blue')
+
+plt.plot(param_range, test_mean, color='green',linestyle='--', marker='s', markersize=5, label='validation accuracy')
+plt.fill_between(param_range, test_mean + test_std, test_mean - test_std, alpha=0.15, color='green')
+
+plt.grid()
+plt.xscale('log')
+plt.legend(loc='lower right')
+plt.xlabel('Parameter C')
+plt.ylabel('Accuracy')
+plt.ylim([0.8, 1.0])
+plt.show()
 
 ############################
 # FINE-TUNING MACHINE LEARNING MODELS VIA GRID SEARCH
